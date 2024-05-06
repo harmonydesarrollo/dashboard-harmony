8f8a026ef4662f55e33dd85353044160cb34d779
SofTest94 <165555006+SofTest94@users.noreply.github.com>
2024-05-06 00:39:09 -0600

    Add files via upload

--- src/components/pages/ServicesHarmony/ServiceListPage.tsx
+++ src/components/pages/ServicesHarmony/ServiceListPage.tsx
@@ -249,7 +249,7 @@ const ServicesList = () => {
             <TableRow>
               <TableCell style={{ fontWeight: 700 }}>NOMBRE</TableCell>
               <TableCell style={{ fontWeight: 700 }}>DESCRIPCIÓN</TableCell>
-              <TableCell style={{ fontWeight: 700 }}>LOGOTIPO</TableCell>
+              <TableCell style={{ fontWeight: 700 }}>IMAGEN</TableCell>
             </TableRow>
           </TableHead>
           <TableBody>
@@ -280,12 +280,19 @@ const ServicesList = () => {
         onPageChange={handleChangePage}
         onRowsPerPageChange={handleChangeRowsPerPage}
       />
-      <Dialog open={modalOpen} onClose={closeModal}>
+      <Dialog
+        open={modalOpen}
+        onClose={(event, reason) => {
+          if (reason !== 'backdropClick') {
+            closeModal();
+          }
+        }}
+      >
         <DialogTitle>{selectedService ? 'Editar Servicio' : 'Agregar Servicio'}</DialogTitle>
         <DialogContent>
           <TextField
             margin="normal"
-            label="Nombre de la Empresa"
+            label="Nombre"
             variant="outlined"
             fullWidth
             value={title}
@@ -293,7 +300,7 @@ const ServicesList = () => {
           />
           <TextField
             margin="normal"
-            label="Resumen"
+            label="Descripción"
             variant="outlined"
             fullWidth
             multiline
