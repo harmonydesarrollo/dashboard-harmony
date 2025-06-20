79d75065ee70682eb38ffd4d9740db911fc8c798
SofTest <softest94@gmail.com>
2025-06-20 15:37:19 -0600

    se agrego la nueva api 20-06-2025

--- src/services/config.ts
+++ src/services/config.ts
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
-const URL_BASE = 'https://harmony-production-ff6ebeae2992.herokuapp.com/';
+// const URL_BASE = 'https://harmony-production-ff6ebeae2992.herokuapp.com/';
+const URL_BASE = 'https://harmony-api-prod-4a08fe78d33b.herokuapp.com/';
 
 const HarmonyApi = axios.create({
   baseURL: URL_BASE,
