12df9a66f6e20065c493e629201cb9406112dbc0
SofTest <softest94@gmail.com>
2026-03-13 18:09:40 -0600

    cambio de url de produccion de heroku

--- src/services/config.ts
+++ src/services/config.ts
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 // const URL_BASE = 'https://harmony-production-ff6ebeae2992.herokuapp.com/';
-const URL_BASE = 'https://harmony-api-prod-4a08fe78d33b.herokuapp.com/';
+// const URL_BASE = 'https://harmony-api-prod-4a08fe78d33b.herokuapp.com/'; // se elimino por falta de pago en heroku
+const URL_BASE = 'https://harmony-api-prod-9e8852c9ba0d.herokuapp.com/';
 // const URL_BASE = 'http://localhost:3000/';
 
 const HarmonyApi = axios.create({
